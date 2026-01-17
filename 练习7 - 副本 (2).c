2653e8fc71d32e8473938837010e31da6975e583
CHUANG <3418732870@qq.com>
2026-01-17 08:46:42 +0800

    结构体与共用体的使用

--- 练习7 - 副本 (2).c
+++ 练习7 - 副本 (2).c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+typedef   struct student {
+	//	int a =0 ;
+		double b ;
+	}st;
 int main(void) {
 	
 	
-	
+	st student2 ;
+	student2.b =4;
 	
 	
 	
