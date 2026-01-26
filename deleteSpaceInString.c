f0eb88d5d39e2b2d57126b55a9ef6fedfcd732da
CHUANG <3418732870@qq.com>
2026-01-26 11:41:30 +0800

    删除字符串中的所有空格

--- deleteSpaceInString.c
+++ deleteSpaceInString.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+// 题目：请编写一个函数，用来删除字符串中的所有空格。
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void)
 {
-    char string[] = "Hwllo Wrold ni hao";
-    int i = 0, j = 0;
-    while (string[i] != '\0')
-    {  // i遍历整个原字符串
+    int i = 0;
+    int j = 0;
+    char string[] = "Hello World wang chuang !";
+    while (string[i] != '\0')  // while循环
+    {
         if (string[i] != ' ')
-        {                             // 只有当前字符不是空格时
-            string[j++] = string[i];  // 先把string[i]赋值给string[j]，然后j自增1
+        {
+            string[j++] = string[i];  // 数组中的j++可改为string【j】
+                                      // j++，只有if成立j才下移
         }
-        i++;  // 不管是不是空格，i每次都自增（继续遍历下一个字符）
+        i++;  // 注意此处的i++不能写在if中，i是一定要自增的，循环结束的条件
     }
-    string[j] = '\0';  // 给新字符串加结束符
+    string[j] = '\0';
+    j = 0;
+    while (string[j] != '\0')
+    {
+        printf("%c", string[j]);
+        j++;  // while循环最后一定勿忘j++以结束循环
+    }
 
     return 0;
 }
