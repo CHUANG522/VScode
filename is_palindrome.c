0c42ebe588aaaacc1c3816ea67f5af4110e9072a
CHUANG <3418732870@qq.com>
2026-01-27 22:14:06 +0800

    Update is_palindrome.c

--- is_palindrome.c
+++ is_palindrome.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+// 请编写函数fmn，该函数的功能是:判断字符串是否为回文
+// 若是，则函数返回1，主函数中输出"YES"，否则返回0，主函数中输出No
 void fun(char a[]);
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -22,12 +23,16 @@ void fun(char a[])
     {
         if (a[i] != a[strlen(a) - i - 1])
         {
-            k = 0;
-            break;
+            k = 0;  // 1，0 判断值
+            break;  // 减少循环次数
         }
-    }
+    }  // 除用函数计算字符长度外，while指针计算
+    // while(*string){
+    //   N++  ;  string ++  ;
+    //  }
+    //   字符串的最后一个\0 的ASCII为0，while的条件只要不是0就执行
     printf(k ? "YES!" : "NO!");
-    /*
+    /*重点：三目printf写法
         if (k == 1)
         {
             printf("YES!");
