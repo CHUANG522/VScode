6be431010997db670585665895ae09d5b91539f7
CHUANG <3418732870@qq.com>
2026-01-30 22:39:45 +0800

    Update exercise  (13) .c

--- exercise  (13) .c
+++ exercise  (13) .c
@@ -3,14 +3,11 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	
-	printf("I LOVE 徐英慧");
-	
-	
-	
-	
-	
-	
-	return 0;
+int main(void)
+{
+    char string[] = "abc";
+    int arr_len = sizeof(string);
+    printf("%c \n", string + 1);
+    printf("%c \n", &string + 1);
+    return 0;
 }
