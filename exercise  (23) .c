d5124915781d372375a9b4e4c7abad1551009aa7
CHUANG <3418732870@qq.com>
2026-02-03 20:50:32 +0800

    Update exercise  (23) .c

--- exercise  (23) .c
+++ exercise  (23) .c
@@ -2,39 +2,40 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void fun(char string[], int m);
 
 int main(void)
 {
     char string[] = "ABCDEFGHIJK";
+    fun(string, 4);
+    return 0;
+}
+void fun(char string[], int m)
+{
     char* p = string;
     int N = 0;
-    int m = 3;
+    int j = 0;
+
     while (*p)
     {
         p++;
         N++;
     }
-    char string_head[N];
-    char string_finish[N];
-    for (int i = 0; i < m; i++)
-    {
-        string_head[i] = string[i];
-    }
-    for (int i = m + 1; i > m && i < N; i++)
-    {
-        string_finish[i] = string[i];
-    }
+    char string_new[N];
     for (int i = 0; i < N; i++)
     {
-        if (i < m)
+        if (i >= m && i < N)
         {
-            string[i] = string_head[i];
+            // printf("%c", string[i]);
+            string_new[j++] = string[i];
         }
-        else if (i > m && i < N)
-        {
-            string[i] = string_finish[i];
-        }
-        printf("%c", string[i]);
     }
-    return 0;
+    for (int i = 0; i < m; i++)
+    {
+        //  printf("%c", string[i]);
+        string_new[j++] = string[i];
+    }
+    string_new[j] = 0;
+    printf("\n");
+    printf("%s", string_new);
 }
