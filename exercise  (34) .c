ad9caa3d859911bd21d1f9594dc00d76ffceda10
CHUANG <3418732870@qq.com>
2026-02-08 22:21:38 +0800

    Update exercise  (34) .c

--- exercise  (34) .c
+++ exercise  (34) .c
@@ -3,14 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+int main(void)
+{
+    double sum = 0;
+    int temp = 0;
+    int N = 11;
+    for (int i = 1; i <= N; i++)
+    {
+        temp += i;
+        sum += 1.0 / temp;
+    }
+
+    return 0;
 }
