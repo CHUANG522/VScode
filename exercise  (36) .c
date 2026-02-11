99feab3e1b52b9e4872e91d5d9ff369d734d600e
CHUANG <3418732870@qq.com>
2026-02-11 22:40:17 +0800

    Update exercise  (36) .c

--- exercise  (36) .c
+++ exercise  (36) .c
@@ -7,11 +7,17 @@ int main(void)
 {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 4, 5, 4, 5};
     int arr_len = sizeof(arr) / sizeof(arr[0]);
-
+    int temp = 0;
     for (int i = 0; i < arr_len; i++)
     {
         for (int j = 0; j < arr_len - 1 - i; j++)
         {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
         }
     }
 
