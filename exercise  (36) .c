436c898b7e7e88269f3ba0f1bb92c5941c214d13
CHUANG <3418732870@qq.com>
2026-02-11 22:10:15 +0800

    Update exercise  (36) .c

--- exercise  (36) .c
+++ exercise  (36) .c
@@ -3,14 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+int main(void)
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 4, 5, 4, 5};
+    int arr_len = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < arr_len; i++)
+    {
+        for (int j = 0; j <) }
+
+    return 0;
 }
