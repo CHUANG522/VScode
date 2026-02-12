f845c98e336cb76d2bf13e559c42037499727ad1
CHUANG <3418732870@qq.com>
2026-02-12 21:52:39 +0800

    数组排列大小

---
+++ int_array_sorter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+// 数组排列大小
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void)
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 2, 4, 5, 4, 5};
+    int arr_len = sizeof(arr) / sizeof(arr[0]);
+    int temp = 0;
+    for (int i = 0; i < arr_len; i++)
+    {
+        for (int j = 0; j < arr_len - 1 - i; j++)  // 每次都少循环一个数
+        {
+            int swar = 1;
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j + 1];    // 交换的步骤，先将A的值存到temp中
+                arr[j + 1] = arr[j];  // 再将B的值赋给A
+
+                arr[j] = temp;  // 最后将temp赋给B
+                swar = 0;
+            }
+        }
+    }
+
+    return 0;
+}
