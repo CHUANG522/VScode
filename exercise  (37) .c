7c6e088fbf0c1501926065ccf3f263fc60f6eb67
CHUANG <3418732870@qq.com>
2026-02-13 20:55:12 +0800

    Update exercise  (37) .c

--- exercise  (37) .c
+++ exercise  (37) .c
@@ -3,14 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+int main(void)
+{
+    char string[] = "****A*BC*DeF*G*****";
+    int arr_len = sizeof(string) / sizeof(string[0]);
+    int j = 0;
+    for (int i = 0; i < arr_len; i++)
+    {
+        if (string[i] <= 'z' && string[i] >= 'a' && string[i] <= 'Z' && string[i] >= 'A' &&
+            string[i - 1] == '*' && string[i + 1] == '*')
+        {
+        }
+    }
+    return 0;
 }
