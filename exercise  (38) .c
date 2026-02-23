885c2756dc537662f3d3e80600fabbe3bf847056
CHUANG <3418732870@qq.com>
2026-02-23 14:08:40 +0800

    Update exercise  (38) .c

--- exercise  (38) .c
+++ exercise  (38) .c
@@ -3,14 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+int main(void)
+{
+    char string[] = "123456";
+    char *p, *t;
+    p = t = string;
+    p = p + 3;
+    printf("%d", (int) (p - t));
+
+    return 0;
 }
