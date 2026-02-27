6705a0861374fa7c97b713117e0f702bd72e9dc7
CHUANG <3418732870@qq.com>
2026-02-27 21:15:49 +0800

    Update exercise  (46) .c

--- exercise  (46) .c
+++ exercise  (46) .c
@@ -3,14 +3,4 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
-}
+int main(void) { return 0; }
