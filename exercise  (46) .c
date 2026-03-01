18fc4c07293feec04310cd7e20e4136212020800
CHUANG <3418732870@qq.com>
2026-03-01 23:38:28 +0800

    Update exercise  (46) .c

--- exercise  (46) .c
+++ exercise  (46) .c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(void) { return 0; }
+struct student
+{
+    float score;
+    struct student* next;
+};
+#define STU struct student
+typedef STU link_node;
+/* run this program using the console pauser or add your own getch, system("pause") or input
+   loop */
+int main(void)
+{
+    link_node stu1;
+    link_node stu2;
+    stu1.score = 39;
+    return 0;
+}
