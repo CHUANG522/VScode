97e0bd4234663265e30348af8b6cc23f30724cb0
CHUANG <3418732870@qq.com>
2026-03-02 23:12:50 +0800

    链表

--- exercise  (46) .c
+++ exercise  (46) .c
@@ -9,10 +9,53 @@ struct student
 typedef STU link_node;
 /* run this program using the console pauser or add your own getch, system("pause") or input
    loop */
+double fun(link_node* h);
+link_node* creat_node(float score);
 int main(void)
 {
     link_node stu1;
     link_node stu2;
     stu1.score = 39;
+    stu2.score = 88;
+
     return 0;
+}
+double fun(link_node* h)
+{
+    if (h == NULL || h->next == NULL)
+    {
+        return -1;
+    }
+    double sum = 0;  // 总值
+    int counter = 0;
+    link_node* p = h->next;
+    while (p != NULL)
+    {
+        counter++;
+        sum += p->score;
+        p = p->next;
+    }
+    return sum / counter;
+}
+link_node* creat_node(float score)
+{
+    link_node* new_node = (link_node*) malloc(sizeof(link_node));
+    if (new_node == NULL)
+    {
+        printf("内存分配失败");
+        exit(1);
+    }
+    new_node->score = score;
+    new_node->next = NULL;
+    return new_node;
+}
+void append_node(link_node* head, float score)
+{
+    link_node* new_node = creat_node(score);
+    link_node* p = head;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = new_node;
 }
