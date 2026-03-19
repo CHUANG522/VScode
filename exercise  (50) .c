2cc06c0d07ddd8731a5b4a3c30e794eee844a8fb
CHUANG <3418732870@qq.com>
2026-03-19 22:12:21 +0800

    求最大功约数，欧几里得算法

--- exercise  (50) .c
+++ exercise  (50) .c
@@ -1,16 +1,23 @@
+// 题目：求两个数的最大公约数,欧几里得算法
 #include <stdio.h>
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(void)
+{
+    int x, y;
+    int min, max = 1;
+    int flag = 0;
+    printf("请输入两个正整数：\n");
+    scanf("%d %d", &x, &y);
+    // xy的值大小不需要比较出来
+    while (y)
+    {
+        int temp = y;
+        y = x % y;  //
+        x = temp;
+    }
 
-int main(void) {
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+    printf("max is %d", x);
+    return 0;
 }
