48c8575e1710108b22e46e573c583d21de562fec
CHUANG <3418732870@qq.com>
2026-04-28 21:31:23 +0800

    练习文件

--- exercise  (49) .c
+++ exercise  (49) .c
@@ -3,14 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) {
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+int main(void)
+{
+    int n, a;
+    long long sum = 0;
+    int temp = 0;
+    if (scanf("%d %d", &n, &a) != 2) printf("输入错误！");
+    for (int i = 1; i <= n; i++)
+    {
+        temp = (long long) temp * 10 + a;
+        sum += temp;
+    }
+    printf("%lld\n", sum);
+    return 0;
 }
